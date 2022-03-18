330cb781bd1c7c633b86b9602aa30b735089c7db
danidoble <dani.doble.d@hotmail.com>
2022-03-18 11:41:08 -0600

    lang app

--- resources/js/lang/fr.js
+++ resources/js/lang/fr.js
@@ -1,7 +1,7 @@
 const fr_lang = {
     "Configuration saved.": "Configuration enregistrée.",
     "Error saving configuration.": "Erreur lors de l'enregistrement de la configuration.",
-    "Exit Kiosk": "Kiosque de sortie",
+    "Exit Kiosk": "Quitter le mode kiosque",
     "Keyword to exit of kiosk mode": "Mot clé pour sortir du mode kiosque",
     "Type the word you want to use to exit kiosk mode.": "Tapez le mot que vous souhaitez utiliser pour quitter le mode kiosque.",
     "Save": "Enregistrer les données",
